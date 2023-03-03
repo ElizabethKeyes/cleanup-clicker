c59c85b151305d719a3f5a899f9f797b7ed752cb
Elizabeth Keyes <liz4momentum@gmail.com>
2023-03-03 14:59:58 -0700

    all 12 core requirements met.

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-const bottle = {
-  karma: 1
-}
-
 const upgrades = [
   {
     name: 'tools',
@@ -12,7 +8,7 @@ const upgrades = [
     price: 2
   },
   {
-    name: 'net',
+    name: 'mask',
     currentKarma: 0,
     maxKarma: 2,
     incrementKarma: 2,
@@ -40,12 +36,17 @@ const intUpgrades = [
   }
 ]
 
+const bottle = {
+  karma: 1
+}
+
 let karma = 0;
 let lifetimeKarma = 0;
 
 let currentKarmaElem = document.getElementById("current-karma")
 let lifetimeKarmaElem = document.getElementById("lifetime-karma")
 
+drawUpgradePrice()
 
 function cleanUp() {
   let upgradesKarma = 0
@@ -55,12 +56,30 @@ function cleanUp() {
   karma += (bottle.karma + upgradesKarma)
   lifetimeKarma += (bottle.karma + upgradesKarma)
   drawKarma()
+  checkButtons()
 }
 
-function drawKarma() {
-  currentKarmaElem.innerText = karma.toString();
-  lifetimeKarmaElem.innerText = lifetimeKarma.toString();
+
+function buyUpgrade(list, name) {
+  let newUpgrade = list.find(u => u.name == name)
+  if (karma >= newUpgrade.price) {
+    newUpgrade.currentKarma = newUpgrade.maxKarma
+    newUpgrade.maxKarma += newUpgrade.incrementKarma
+    karma -= newUpgrade.price
+    newUpgrade.quantity++
+    newUpgrade.price += Math.floor(newUpgrade.price / 10)
+
+  } else window.alert("You don't have enough karma for that!")
+  console.log(newUpgrade);
+
+  drawKarma()
+  drawStats()
+  drawUpgradeStats()
+  drawUpgradePrice()
+  checkButtons()
 }
+
+// SECTION draw functions
 
 function drawStats() {
   let statsElem = document.getElementById("stats")
@@ -69,36 +88,60 @@ function drawStats() {
   upgrades.forEach(u => upgradesKarma += u.currentKarma)
   intUpgrades.forEach(u => intUpgradeKarma += u.currentKarma)
   let template = `
-<h5>${bottle.karma + upgradesKarma}</h5>
+  <h5>${bottle.karma + upgradesKarma}</h5>
 <h5>${intUpgradeKarma}</h5>`
   statsElem.innerHTML = template
 }
 
-function buyUpgrade(name) {
-  let newUpgrade = upgrades.find(u => u.name == name)
-  if (karma >= newUpgrade.price) {
-    newUpgrade.currentKarma = newUpgrade.maxKarma
-    newUpgrade.maxKarma += newUpgrade.incrementKarma
-    karma -= newUpgrade.price
-    newUpgrade.quantity++
-  } else window.alert("You don't have enough karma for that!")
-  console.log(newUpgrade);
-  drawKarma()
-  drawStats()
+function drawKarma() {
+  currentKarmaElem.innerText = karma.toString();
+  lifetimeKarmaElem.innerText = lifetimeKarma.toString();
 }
 
-function buyIntUpgrade(name) {
-  let newUpgrade = intUpgrades.find(u => u.name == name)
-  if (karma >= newUpgrade.price) {
-    newUpgrade.currentKarma = newUpgrade.maxKarma
-    newUpgrade.maxKarma += newUpgrade.incrementKarma
-    karma -= newUpgrade.price
-    newUpgrade.quantity++
-  } else window.alert("You don't have enough karma for that!")
-  console.log(newUpgrade);
-  drawKarma()
-  drawStats()
+function drawUpgradePrice() {
+  let upgradePriceElem = document.getElementById("upgrade-price")
+  let template = `
+    <h5 class = "mb-3">${upgrades[0].price}</h5>
+    <h5 class = "mb-3">${upgrades[1].price}</h5>
+    <h5 class = "mb-3">${intUpgrades[0].price}</h5>
+    <h5 class = "mb-3">${intUpgrades[1].price}</h5>`
+
+  upgradePriceElem.innerHTML = template
 }
+
+function drawUpgradeStats() {
+  let upgradeStatsElem = document.getElementById("upgrade-stats")
+  let template = `
+    <h5>${upgrades[0].quantity}</h5>
+    <h5>${upgrades[1].quantity}</h5>
+    <h5>${intUpgrades[0].quantity}</h5>
+    <h5>${intUpgrades[1].quantity}</h5>`
+  upgradeStatsElem.innerHTML = template
+}
+
+function checkButtons() {
+  for (let i = 0; i < upgrades.length; i++) {
+    if (karma < upgrades[i].price) {
+      document.getElementById(upgrades[i].name).style.pointerEvents = 'none';
+      document.getElementById(upgrades[i].name).classList.add("text-danger");
+    } else {
+      document.getElementById(upgrades[i].name).style.pointerEvents = 'auto';
+      document.getElementById(upgrades[i].name).classList.remove("text-danger");
+    }
+  }
+
+  for (let i = 0; i < intUpgrades.length; i++) {
+    if (karma < intUpgrades[i].price) {
+      document.getElementById(intUpgrades[i].name).style.pointerEvents = 'none';
+      document.getElementById(intUpgrades[i].name).classList.add("text-danger");
+    } else {
+      document.getElementById(intUpgrades[i].name).style.pointerEvents = 'auto';
+      document.getElementById(intUpgrades[i].name).classList.remove("text-danger");
+    }
+  }
+}
+
+// SECTION interval
 
 function intervalUpgrades() {
   let intUpgradeKarma = 0
@@ -109,6 +152,7 @@ function intervalUpgrades() {
   console.log(intUpgradeKarma, 'intUpgradeKarma')
   drawKarma()
   drawStats()
+  checkButtons()
 }
 
 setInterval(intervalUpgrades, 1000)
