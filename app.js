bae7a0fe8c42912d14a639e19b0cc94eb0f58106
Elizabeth Keyes <liz4momentum@gmail.com>
2023-03-03 12:46:20 -0700

    need to fix interval function

--- app.js
+++ app.js
@@ -7,21 +7,50 @@ const upgrades = [
     name: 'tools',
     currentKarma: 0,
     maxKarma: 1,
+    incrementKarma: 1,
+    quantity: 0,
     price: 2
+  },
+  {
+    name: 'net',
+    currentKarma: 0,
+    maxKarma: 2,
+    incrementKarma: 2,
+    quantity: 0,
+    price: 4
+  },
+]
+
+const intUpgrades = [
+  {
+    name: 'volunteer',
+    currentKarma: 0,
+    maxKarma: 1,
+    incrementKarma: 1,
+    quantity: 0,
+    price: 10
+  },
+  {
+    name: 'boat',
+    currentKarma: 0,
+    maxKarma: 2,
+    incrementKarma: 2,
+    quantity: 0,
+    price: 20
   }
 ]
 
 let karma = 0;
 let lifetimeKarma = 0;
-let upgradesKarma = 0
 
 let currentKarmaElem = document.getElementById("current-karma")
 let lifetimeKarmaElem = document.getElementById("lifetime-karma")
 
 
 function cleanUp() {
+  let upgradesKarma = 0
   upgrades.forEach(u => {
-    upgradesKarma = u.currentKarma
+    upgradesKarma += u.currentKarma
   })
   karma += (bottle.karma + upgradesKarma)
   lifetimeKarma += (bottle.karma + upgradesKarma)
@@ -33,14 +62,49 @@ function drawKarma() {
   lifetimeKarmaElem.innerText = lifetimeKarma.toString();
 }
 
+function drawStats() {
+  let statsElem = document.getElementById("stats")
+  let upgradesKarma = 0
+  upgrades.forEach(u => upgradesKarma += u.currentKarma)
+  let template = `
+<h5>${bottle.karma + upgradesKarma}</h5>
+<h5>0</h5>`
+  statsElem.innerHTML = template
+}
+
 function buyUpgrade(name) {
-  let newUpgrade = upgrades.find(u => u.name = name)
+  let newUpgrade = upgrades.find(u => u.name == name)
   if (karma >= newUpgrade.price) {
     newUpgrade.currentKarma = newUpgrade.maxKarma
-    newUpgrade.maxKarma += 1
+    newUpgrade.maxKarma += newUpgrade.incrementKarma
     karma -= newUpgrade.price
-  }
+    newUpgrade.quantity++
+  } else window.alert("You don't have enough karma for that!")
   console.log(newUpgrade);
   drawKarma()
-
+  drawStats()
 }
+
+function buyIntUpgrade(name) {
+  let newUpgrade = intUpgrades.find(u => u.name == name)
+  if (karma >= newUpgrade.price) {
+    newUpgrade.currentKarma = newUpgrade.maxKarma
+    newUpgrade.maxKarma += newUpgrade.incrementKarma
+    karma -= newUpgrade.price
+    newUpgrade.quantity++
+  } else window.alert("You don't have enough karma for that!")
+  console.log(newUpgrade);
+}
+
+// REVIEW interval is adding 2 instead of just one
+function intervalUpgrades() {
+  let intUpgradeKarma = 0
+  intUpgrades.forEach(i => {
+    intUpgradeKarma += i.currentKarma
+    karma += intUpgradeKarma
+  })
+  console.log(intUpgradeKarma)
+  drawKarma()
+}
+
+setInterval(intervalUpgrades, 1000)
